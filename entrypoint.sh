ea96acddb178aa574601e58f57ac19286433c11f
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2020-03-14 01:31:00 -0400

    Use gsutil -m for faster rsync

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,5 +20,5 @@ echo "$INPUT_SERVICE_KEY" | base64 -d > "$HOME"/service_key.json
 
 gcloud auth activate-service-account --key-file="$HOME"/service_key.json --project "$INPUT_PROJECT"
 
-gsutil rsync -R "$INPUT_BUILD_FOLDER" gs://"$INPUT_BUCKET_NAME"
+gsutil -m rsync -R "$INPUT_BUILD_FOLDER" gs://"$INPUT_BUCKET_NAME"
 gsutil web set -m "$INPUT_HOME_PAGE_PATH" -e "$INPUT_ERROR_PAGE_PATH" gs://"$INPUT_BUCKET_NAME"
